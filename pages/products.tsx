093fc7c1aa483bb55e7fbf036e6bb5c0526751a0
alejandro <alexandros2707@gmail.com>
2021-10-26 23:04:36 -0400

    ✨ add products page

---
+++ pages/products.tsx
@@ -0,0 +1,15 @@
+function Products() {
+  return (
+    <>
+      <h1>CoolProduct 1</h1>
+      <p>This is a very cool product</p>
+      <p>$2.50</p>
+      <hr />
+      <h1>CoolProduct 2</h1>
+      <p>This is an even cooler product</p>
+      <p>$250.00</p>
+    </>
+  )
+}
+
+export default Products
